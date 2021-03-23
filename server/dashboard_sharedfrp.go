326fe2318bc737f02ec79f2a26e375e34e7e8ed6
tuuzed <liuyonghui.job@gmail.com>
2021-03-23 16:31:54 +0800

    v0.36.2-sharedfrp-02

--- server/dashboard_sharedfrp.go
+++ server/dashboard_sharedfrp.go
@@ -7,28 +7,11 @@ import (
 	"github.com/fatedier/frp/pkg/util/version"
 )
 
-type ServerInfo struct {
-	Version           string `json:"version"`
-	BindPort          int    `json:"bind_port"`
-	BindUDPPort       int    `json:"bind_udp_port"`
-	VhostHTTPPort     int    `json:"vhost_http_port"`
-	VhostHTTPSPort    int    `json:"vhost_https_port"`
-	KCPBindPort       int    `json:"kcp_bind_port"`
-	SubdomainHost     string `json:"subdomain_host"`
-	MaxPoolCount      int64  `json:"max_pool_count"`
-	MaxPortsPerClient int64  `json:"max_ports_per_client"`
-	HeartBeatTimeout  int64  `json:"heart_beat_timeout"`
+type ServerInfo serverInfoResp
 
-	TotalTrafficIn  int64            `json:"total_traffic_in"`
-	TotalTrafficOut int64            `json:"total_traffic_out"`
-	CurConns        int64            `json:"cur_conns"`
-	ClientCounts    int64            `json:"client_counts"`
-	ProxyTypeCounts map[string]int64 `json:"proxy_type_count"`
-}
-
-func (svr *Service) GetServerInfo() (serverInfo ServerInfo) {
+func (svr *Service) GetServerInfo() (serverInfo *ServerInfo) {
 	serverStats := mem.StatsCollector.GetServer()
-	serverInfo = ServerInfo{
+	serverInfo = &ServerInfo{
 		Version:           version.Full(),
 		BindPort:          svr.cfg.BindPort,
 		BindUDPPort:       svr.cfg.BindUDPPort,
